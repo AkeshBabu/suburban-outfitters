ff43467eabc823f2e46984e58e769f15744fd7d9
AkeshBabu <akesh.anumala@gmail.com>
2023-11-30 15:26:19 -0700

    add inventory table in db, updated admin pages- view products, add product

---
+++ src/product-edit.php
@@ -0,0 +1,267 @@
+<?php
+require_once 'checksession.php';
+require_once 'conn.php';
+
+$inventory_id = $_GET['inventory_id'] ?? 0;
+
+$conn = new mysqli($hn, $un, $pw, $db);
+if ($conn->connect_error) die("Connection failed: " . $conn->connect_error);
+
+if ($_SERVER["REQUEST_METHOD"] == "POST" && $inventory_id) {
+    $product_id = $conn->real_escape_string($_POST['product_id']);
+    $vendor_id = $conn->real_escape_string($_POST['vendor_id']);
+    $quantity = $conn->real_escape_string($_POST['quantity']);
+    $inventory_date = $conn->real_escape_string($_POST['inventory_date']);
+
+    $stmt = $conn->prepare("UPDATE inventory SET product_id=?, vendor_id=?, quantity=?, inventory_date=? WHERE inventory_id=?");
+    $stmt->bind_param("iiisi", $product_id, $vendor_id, $quantity, $inventory_date, $inventory_id);
+    if ($stmt->execute()) {
+        echo "<script>alert('Product updated successfully.'); window.location.href='product-list.php';</script>";
+    } else {
+        echo "<script>alert('Error updating product.'); window.location.href='product-edit.php?inventory_id=$inventory_id';</script>";
+    }
+    $stmt->close();
+}
+
+if ($inventory_id) {
+    $stmt = $conn->prepare("SELECT * FROM inventory WHERE inventory_id = ?");
+    $stmt->bind_param("i", $inventory_id);
+    $stmt->execute();
+    $result = $stmt->get_result();
+    $product = $result->fetch_assoc();
+    $stmt->close();
+}
+
+$conn->close();
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Register</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/css/bootstrap.min.css" rel="stylesheet">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.3/css/all.min.css">
+    <link rel="stylesheet" href="stylesheets/style.css">
+    <link href='https://fonts.googleapis.com/css?family=Montserrat' rel='stylesheet'>
+    <style>
+        .registration-form {
+            border: 2px solid #dee2e6 !important;
+            border-radius: 5px !important;
+            padding: 20px !important;
+            max-width: 600px !important;
+            margin: 50px auto !important;
+        }
+
+        .form-group {
+            margin-bottom: 15px !important;
+        }
+
+        .registration-form label {
+            margin-bottom: 5px !important;
+        }
+
+        .registration-form input[type="checkbox"] {
+            margin-top: 3px !important;
+        }
+
+        .registration-form button {
+            width: unset !important;
+        }
+
+        .hidden {
+            display: none;
+        }
+
+        .btn-primary {
+            background-color: black !important;
+        }
+    </style>
+</head>
+
+<body class="d-flex flex-column min-vh-100">
+
+    <header>
+        <nav class="navbar navbar-expand-lg navbar-light ">
+            <div class="container-fluid">
+                <a class="navbar-brand" href="homepage.php">
+                    <img src="images/Suburban-gif.gif" width="50%" height="50%">
+                </a>
+                <h1 style="font-weight:600;" class="mx-auto">The Official Store of Suburban Outfitters</h1>
+                <ul class="navbar-nav">
+                    <li id="searchBox" class="nav-item">
+                        <form action="view-products.php" class="d-flex" method="GET">
+                            <div class="input-group">
+                                <input style="border: double;" class="form-control" type="search" name="search"
+                                    placeholder="Search" aria-label="Search">
+                                <button style="border: double; background-color: white;" type="submit"> <img
+                                        src="https://cdn-icons-png.flaticon.com/128/2811/2811806.png" width="30px"
+                                        height="30px"></button>
+                            </div>
+                        </form>
+                    </li>
+                    <li id="profileIcon" class="nav-item">
+                        <a class="nav-link" href="profile.php">
+                            <img src="https://cdn-icons-png.flaticon.com/128/64/64572.png" width="30px" height="30px">
+                        </a>
+                    </li>
+                    <li id="cartIcon" class="nav-item">
+                        <a class="nav-link" href="cart.php">
+                            <img src="https://cdn-icons-png.flaticon.com/128/253/253298.png" width="30px" height="30px">
+                        </a>
+                    </li>
+                </ul>
+            </div>
+        </nav>
+    </header>
+
+	<div class="container">
+		<br><br>
+			<h2 class="text-center">Edit Product Details</h2>
+			<div class="registration-form">
+				<form action="product-edit.php?inventory_id=<?php echo $inventory_id; ?>" method="post">
+					<div class="form-group">
+						<label for="product_id">Product ID:</label>
+						<input type="text" class="form-control" id="product_id" name="product_id" value="<?php echo htmlspecialchars($product['product_id'] ?? ''); ?>" readonly>
+					</div>
+
+					<div class="form-group">
+						<label for="vendor_id">Vendor ID:</label>
+						<input type="text" class="form-control" id="vendor_id" name="vendor_id" value="<?php echo htmlspecialchars($product['vendor_id'] ?? ''); ?>" readonly>
+					</div>
+
+					<div class="form-group">
+						<label for="quantity">Quantity:</label>
+						<input type="number" class="form-control" id="quantity" name="quantity" value="<?php echo htmlspecialchars($product['quantity'] ?? ''); ?>" required>
+					</div>
+
+					<div class="form-group">
+						<label for="inventory_date">Inventory Date:</label>
+						<input type="date" class="form-control" id="inventory_date" name="inventory_date" value="<?php echo htmlspecialchars($product['inventory_date'] ?? ''); ?>" required>
+					</div>
+
+					<button type="submit" class="btn btn-primary">Update</button>
+				</form>
+			</div>
+		</div>
+
+
+    <!-- Footer -->
+    <footer class="mt-auto">
+        <div class="container py-4" style="text-align: center;">
+            <div class="row">
+                <div class="col-md-3">
+                    <a href="#" id="contactusModalTrigger">Contact Us</a>
+                </div>
+                <div class="col-md-3">
+                    <a href="#" id="LocationHoursTrigger">Location and Hours</a>
+                </div>
+                <div class="col-md-3">
+                    <a href="#" id="privacyTermsModalTrigger">Privacy and Terms</a>
+                </div>
+                <div class="col-md-3">
+                    <a href="https://www.instagram.com/" target=" _blank"><i class="fab fa-instagram"></i></a>
+                    <a href="https://www.facebook.com/" target=" _blank"><i class="fab fa-facebook-f"></i></a>
+                    <a href="https://twitter.com/" target=" _blank"><i class="fab fa-twitter"></i></a>
+                </div>
+            </div>
+        </div>
+        <div class="text-center p-3" style="background-color: rgba(0, 0, 0, 0.2);">
+            <a tabindex="0" style="color: red;">Copyright 2023 © Suburban Outfitters </a>
+        </div>
+    </footer>
+
+    <!-- Modal for Contact Us -->
+    <div class="modal fade" id="contactUsModal" tabindex="-1" role="dialog" aria-labelledby="contactUsModalLabel"
+        aria-hidden="true" style="padding-right: 17px; display: none;">
+        <div class="modal-dialog" role="document">
+            <div class="modal-content">
+                <div class="modal-header">
+                    <h5 class="modal-title" id="contactUsModalLabel">Contact Us</h5>
+                    <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                        <span aria-hidden="true">&times;</span>
+                    </button>
+                </div>
+                <div class="modal-body">
+
+                    <p>Email us at: <a href="mailto: suburbanoutfitters@gmail.com">suburbanoutfitters@gmail.com</a></p>
+                    <p>Call us at: <a href="tel:+123456789">1234567890</a>
+                </div>
+                <div class="modal-footer">
+                    <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
+                </div>
+            </div>
+        </div>
+    </div>
+
+    <!-- Modal for Location and Hours -->
+    <div class="modal fade show" id="locationHoursModal" tabindex="-1" role="dialog"
+        aria-labelledby="locationHoursModalLabel" aria-hidden="true" style="padding-right: 17px; display: none;">
+        <div class="modal-dialog" role="document">
+            <div class="modal-content">
+                <div class="modal-header">
+                    <h5 class="modal-title" id="locationHoursModalLabel">Location and Hours</h5>
+                    <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                        <span aria-hidden="true">&times;</span>
+                    </button>
+                </div>
+                <div class="modal-body">
+
+                    <p>We are open 24x7, anywhere and everywhere!</p>
+                </div>
+                <div class="modal-footer">
+                    <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
+                </div>
+            </div>
+        </div>
+    </div>
+
+    <!-- Modal for Privacy and Terms -->
+    <div class="modal fade show" id="privacyTermsModal" tabindex="-1" role="dialog"
+        aria-labelledby="privacyTermsModalLabel" style="padding-right: 17px; display: none;" aria-hidden="true">
+        <div class="modal-dialog" role="document">
+            <div class="modal-content">
+                <div class="modal-header">
+                    <h5 class="modal-title" id="privacyTermsModalLabel">Privacy and Terms</h5>
+                    <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                        <span aria-hidden="true">&times;</span>
+                    </button>
+                </div>
+                <div class="modal-body">
+                    <p style="font-size:14px;">Created and Maintained by DevTeam @ Suburban Outfitters.</p>
+                </div>
+                <div class="modal-footer">
+                    <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
+                </div>
+            </div>
+        </div>
+    </div>
+
+    <script>
+        // JavaScript to open the Privacy and Terms modal
+        document.getElementById('privacyTermsModalTrigger').addEventListener('click', function () {
+            var myModal = new bootstrap.Modal(document.getElementById('privacyTermsModal'));
+            myModal.show();
+        });
+        document.getElementById('contactusModalTrigger').addEventListener('click', function () {
+            var myModal = new bootstrap.Modal(document.getElementById('contactUsModal'));
+            myModal.show();
+        });
+        document.getElementById('LocationHoursTrigger').addEventListener('click', function () {
+            var myModal = new bootstrap.Modal(document.getElementById('locationHoursModal'));
+            myModal.show();
+        });
+    </script>
+    <script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0/dist/js/bootstrap.bundle.min.js"></script>
+    <script>
+        // Function to close the modal with the ID "myModal"
+        function closeModal() {
+            $('#myModal').modal('hide');
+        }
+    </script>
+</body>
+
+</html>
